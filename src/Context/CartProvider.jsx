a717fa9ec681e273564d314b8ce4e14e9b92dc42
Manimala S <manimala3214@gmail.com>
2025-11-19 10:50:14 +0530

    fourth commit

--- src/Context/CartProvider.jsx
+++ src/Context/CartProvider.jsx
@@ -1,44 +1,58 @@
-import { useState } from "react"
-import CartContext from "./CartContext";
+import { useState, useEffect } from 'react';
+import CartContext from './CartContext';
 import 'react-toastify/dist/ReactToastify.css';
-import { toast, ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from 'react-toastify';
 
-const CartProvider = ({ children })=> {
-    const [count, setCount] = useState(0);
-    const [cartItem, setCartItem] = useState([]);
-    const addToCart =  (item)=> {
-        const exists = cartItem.find(cart=> cart._id === item._id)
-        if(exists) {
-            toast.warning("Product already in cart!", {
-                position: "top-center",
-                autoClose: 1000,
-                theme: "dark"
-            });
-        }
-        else{
-            setCartItem(prevItem=> [...prevItem, item]);
-            setCount ((prevCount)=> prevCount + 1);
-            toast.success("Product added to cart!", {
-                position: "top-center",
-                autoClose: 1000,
-                theme: "dark"
-            });
-        }
-    };
-    const removeFromCart = (id)=> {
-        setCartItem((prevItem)=> prevItem.filter(item=>item._id !==id));
-        setCount((prevCount)=> (prevCount>0 ? prevCount-1: 0));
-        toast.info("Product removed from cart!", {
-            position: "top-center",
-            autoClose: 1000,
-            theme: "dark"
-        })
-    };
-    return (
-        <CartContext.Provider value = {{count, cartItem, addToCart, removeFromCart}}>
-            {children}
-            <ToastContainer />
-        </CartContext.Provider>
-    )
+const CartProvider = ({ children }) => {
+  const [count, setCount] = useState(() => {
+    const savedCount = localStorage.getItem('cart_count');
+    return savedCount ? JSON.parse(savedCount) : 0;
+  });
+
+  const [cartItem, setCartItem] = useState(() => {
+    const saved = localStorage.getItem('cart_items');
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  useEffect(() => {
+        localStorage.setItem("cart_items", JSON.stringify(cartItem));
+        localStorage.setItem("cart_count", JSON.stringify(count));
+    }, [cartItem, count]);
+
+  const addToCart = (item) => {
+    const exists = cartItem.find((cart) => cart._id === item._id);
+    if (exists) {
+      toast.warning('Product already in cart!', {
+        position: 'top-center',
+        autoClose: 1000,
+        theme: 'dark',
+      });
+    } else {
+      setCartItem((prevItem) => [...prevItem, item]);
+      setCount((prevCount) => prevCount + 1);
+      toast.success('Product added to cart!', {
+        position: 'top-center',
+        autoClose: 1000,
+        theme: 'dark',
+      });
+    }
+  };
+  const removeFromCart = (id) => {
+    setCartItem((prevItem) => prevItem.filter((item) => item._id !== id));
+    setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : 0));
+    toast.info('Product removed from cart!', {
+      position: 'top-center',
+      autoClose: 1000,
+      theme: 'dark',
+    });
+  };
+  return (
+    <CartContext.Provider
+      value={{ count, cartItem, addToCart, removeFromCart }}
+    >
+      {children}
+      <ToastContainer />
+    </CartContext.Provider>
+  );
 };
 export default CartProvider;
